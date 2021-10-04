690fdaabdc51e0e957acfc39775ab8ed35a66cc8
Matteo Magni <matteo.magni@quix.it>
2021-10-04 11:36:39 +0200

    fix terraform runner labels definition

--- image/init.sh
+++ image/init.sh
@@ -55,7 +55,7 @@ sudo apt-get install -y 'stackdriver-agent=6.*'
 ## GitHub
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt-get update
+sudo apt-get update
 sudo apt-get -y install gh
 
 ## Runner
